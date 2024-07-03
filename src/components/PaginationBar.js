6d708a2c9b875164074535b5c311e105fb75815e
Danh <nguyenhoangcongdanh2075@gmail.com>
2024-07-03 11:45:08 +0700

    commit

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -1,14 +1,22 @@
 import React from "react";
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-
-const PaginationBar = ({ pageNum, setPageNum, totalPageNum }) => {
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useDispatch } from "react-redux";
+import { fetchData } from "../server/homepage/homeSlice";
+const PaginationBar = ({ query, totalPageNum, pageNum }) => {
+  const dispatch = useDispatch();
   const handleChange = (event, value) => {
-    setPageNum(value);
+    dispatch(fetchData({ page: value, query: query }));
   };
   return (
     <Stack spacing={2}>
-      <Pagination count={totalPageNum} page={pageNum} onChange={handleChange} showFirstButton showLastButton />
+      <Pagination
+        page={pageNum}
+        count={totalPageNum}
+        onChange={handleChange}
+        showFirstButton
+        showLastButton
+      />
     </Stack>
   );
 };
--- src/pages/ReadingPage.js
+++ src/pages/ReadingPage.js
@@ -1,75 +1,68 @@
-import React, { useState, useEffect } from "react";
-import { Container, Button, Box, Card, Stack, CardMedia, CardActionArea, Typography, CardContent } from "@mui/material";
+import React, { useEffect } from "react";
+import {
+  Container,
+  Button,
+  Box,
+  Card,
+  Stack,
+  CardMedia,
+  Typography,
+  CardContent,
+} from "@mui/material";
 import { ClipLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import api from "../apiService";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../server/reading/readingSlice";
 const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
 const ReadingPage = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [removedBookId, setRemovedBookId] = useState("");
-  const navigate = useNavigate()
-
+  const dataReading = useSelector((state) => state.reading);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleClickBook = (bookId) => {
     navigate(`/books/${bookId}`);
   };
 
-  const removeBook = (bookId) => {
-    setRemovedBookId(bookId);
-  };
+  useEffect(() => {
+    dispatch(fetchData());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
-    if (removedBookId) return;
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await api.get(`/favorites`);
-        setBooks(res.data);
-      } catch (error) {
-        toast(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [removedBookId]);
-
-  useEffect(() => {
-    if (!removedBookId) return;
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        await api.delete(`/favorites/${removedBookId}`);
-        toast.success("The book has been removed");
-        setRemovedBookId("");
-      } catch (error) {
-        toast(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [removedBookId]);
+    if (dataReading.isSuccess && dataReading.status === "idle") {
+      toast.success("The book has been removed");
+    } else if (dataReading.status === "error") {
+      toast("something Wrong");
+    }
+  }, [dataReading.isSuccess, dataReading.status]);
 
   return (
     <Container>
-      <Typography variant="h3" sx={{ textAlign: "center" }} m={3}>Book Store</Typography>
-      {loading ? (
-        <Box sx={{ textAlign: "center", color: "primary.main" }} >
+      <Typography variant="h3" sx={{ textAlign: "center" }} m={3}>
+        Book Store
+      </Typography>
+      {dataReading.status === "loading" ? (
+        <Box sx={{ textAlign: "center", color: "primary.main" }}>
           <ClipLoader color="inherit" size={150} loading={true} />
         </Box>
       ) : (
-        <Stack direction="row" spacing={2} justifyContent="space-around" flexWrap={"wrap"}>
-          {books.map((book) => (
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="space-around"
+          flexWrap={"wrap"}
+        >
+          {dataReading.books?.map((book) => (
             <Card
               key={book.id}
               sx={{
                 width: "12rem",
                 height: "27rem",
                 marginBottom: "2rem",
-              }}>
-              <CardActionArea>
+              }}
+            >
+              <Card sx={{ position: "relative", height: "100%" }}>
                 <CardMedia
                   component="img"
                   image={`${BACKEND_API}/${book.imageLink}`}
@@ -85,23 +78,26 @@ const ReadingPage = () => {
                   </Typography>
                   <Button
                     sx={{
-                      position: "absolute", top: "5px", right: "5px",
-                      backgroundColor: "secondary.light", color: "secondary.contrastText",
-                      padding: "0", minWidth: "1.5rem"
+                      position: "absolute",
+                      top: "5px",
+                      right: "5px",
+                      backgroundColor: "secondary.light",
+                      color: "secondary.contrastText",
+                      padding: "0",
+                      minWidth: "1.5rem",
                     }}
                     size="small"
-                    onClick={() => removeBook(book.id)}
+                    onClick={() => dispatch(fetchData(book.id))}
                   >
                     &times;
                   </Button>
                 </CardContent>
-
-              </CardActionArea>
+              </Card>
             </Card>
           ))}
         </Stack>
       )}
-    </Container >
+    </Container>
   );
 };
 
